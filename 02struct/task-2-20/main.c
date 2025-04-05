63fb86dc6678f6a235fce01969480571704fa815
vDenight <mikrybczynski@gmail.com>
2025-04-05 22:27:59 +0200

    task 2-20 having issues

--- 02struct/task-2-20/main.c
+++ 02struct/task-2-20/main.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 struct message_t
 {
@@ -23,7 +24,17 @@ int main(void) {
     readFilename(input);
 
     int loaded_amount = load_data(message_array, 100, input);
-    printf("Loaded %d messages\n", loaded_amount);
+
+    if (loaded_amount == -2) {
+        printf("Couldn't open file");
+        return 4;
+    }
+
+    if (loaded_amount == -3) {
+        printf("File corrupted");
+        return 6;
+    }
+
     decode_message(message_array, loaded_amount, msg, 1101);
 
     printf("%s", msg);
@@ -39,14 +50,17 @@ int load_data(struct message_t *cp, int size, const char *filename) {
 
     FILE *fp = NULL;
     fp = fopen(filename, "rb");
+
     if (fp == NULL)
         return -2;
 
     unsigned int amount_of_bytes = 0;
     fread(&amount_of_bytes, sizeof(unsigned int), 1, fp);
 
-    if (amount_of_bytes  % 24 != 0)
+    if (amount_of_bytes  % 24 != 0) {
+        fclose(fp);
         return -3;
+    }
 
     int read_amount = (int) fread(cp, sizeof(struct message_t), size, fp);
 
@@ -74,19 +88,19 @@ int decode_message(const struct message_t *cp, int size, char *msg, int text_siz
         void *char_pointer = &current_message_pointer->a;
         void *int_pointer = &current_message_pointer->c;
 
-        for (int j = 0; j < sizeof(struct message_t); j++) {
+        for (int j = 0; j < (int) sizeof(struct message_t); j++) {
 
             if (current_text_size == text_size - 1) {
                 *msg = '\0';
                 return 0;
             }
 
-            if ((void*) current_point >= int_pointer && (void*) current_point < int_pointer + sizeof(int)) {
+            if ((void*) current_point >= int_pointer && (void*) current_point < (void*) ((char*) int_pointer + 4)) {
                 current_point ++;
                 continue;
             }
 
-            if ((void*) current_point >= double_pointer && (void*) current_point < double_pointer + sizeof(double)) {
+            if ((void*) current_point >= double_pointer && (void*) current_point < (void*) ((char*) double_pointer + 8)) {
                 current_point ++;
                 continue;
             }
@@ -95,7 +109,7 @@ int decode_message(const struct message_t *cp, int size, char *msg, int text_siz
                 current_point ++;
                 continue;
             }
-            *msg = *current_point;
+            *msg = *current_point; // 	Assigned value is garbage or undefined
             msg++;
             current_text_size ++;
             current_point ++;
