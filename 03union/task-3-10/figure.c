c0ab25dd8dc9a8c9541482ceb45b7e2d67898ebd
vDenight <vdenightpl@gmail.com>
2025-04-17 15:54:04 +0200

    union task 10 (unfinished) still upgrading functions

--- 03union/task-3-10/figure.c
+++ 03union/task-3-10/figure.c
@@ -1,24 +1,96 @@
 #include "figure.h"
 
+#include <float.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 float area_triangle(const struct triangle_t *t) {
-    return fabsf((float)(t->p1.x * (t->p2.y - t->p3.y) +
-                         t->p2.x * (t->p3.y - t->p1.y) +
-                         t->p3.x * (t->p1.y - t->p2.y))) * 0.5f;
+
+    if (t == NULL)
+        return -1;
+
+    // check overflow on each part of the logic
+
+    double result_part_1 = t->p2.y;
+    double result_part_2 = t->p3.y;
+    double result_part_3 = t->p1.y;
+
+    if (result_part_1 > FLT_MAX + (double) t->p3.y || result_part_1 < (-FLT_MAX) + (double) t->p3.y ||
+        result_part_2 > FLT_MAX + (double) t->p2.y || result_part_2 < (-FLT_MAX) + (double) t->p2.y ||
+        result_part_3 > FLT_MAX + (double) t->p1.y || result_part_3 < (-FLT_MAX) + (double) t->p1.y) {
+        return -1;
+    }
+
+    result_part_1 -= t->p3.y;
+    result_part_2 -= t->p1.y;
+    result_part_3 -= t->p2.y;
+
+    if (fabs(result_part_1) > fabs(FLT_MAX / (double) t->p1.x) ||
+        fabs(result_part_2) > fabs(FLT_MAX / (double) t->p2.x) ||
+        fabs(result_part_3) > fabs(FLT_MAX / (double) t->p3.x)) {
+        return -1;
+    }
+
+    result_part_1 *= t->p1.x;
+    result_part_2 *= t->p2.x;
+    result_part_3 *= t->p3.x;
+
+    if (result_part_1 > FLT_MAX - result_part_2 || result_part_1 < (-FLT_MAX) - result_part_2) {
+        return -1;
+    }
+
+    result_part_1 += result_part_2;
+
+    if (result_part_2 > FLT_MAX - result_part_3 || result_part_2 < (-FLT_MAX) - result_part_3) {
+        return -1;
+    }
+
+    result_part_1 += result_part_3;
+
+    return result_part_1 == 0 ? -1 : (float) fabs(result_part_1) * 0.5f;
 }
 
 float area_rectangle(const struct rectangle_t *r) {
-    return (float) r->width * (float) r->height;
+
+    if (r == NULL)
+        return -1;
+
+    if ((double) r->width > FLT_MAX / (double) r->height + 1)
+        return -1;
+
+    if (r->height <= 0 || r->width <= 0)
+        return -1;
+
+    double result = (double) r->width * (double) r->height;
+
+    if (result > FLT_MAX)
+        return -1;
+
+    return (float) result;
 }
 
 float area_circle(const struct circle_t *c) {
-    return M_PI * c->r * c->r;
+
+    if (c == NULL)
+        return -1;
+
+    if (c->r <= 0)
+        return -1;
+
+    double result = M_PI * c->r * c->r;
+
+    if (result > FLT_MAX)
+        return -1;
+
+    return (float) result;
 }
 
 float area_figure(const struct figure_t *f) {
+
+    if (f == NULL)
+        return -1;
+
     switch (f->type) {
         case TRIANGLE:
             return area_triangle(&f->triangle);
@@ -26,22 +98,23 @@ float area_figure(const struct figure_t *f) {
             return area_rectangle(&f->rect);
         case CIRCLE:
             return area_circle(&f->circ);
+        default:
+            return -1;
     }
-    return 0;
 }
 
 void display_rectangle(const struct rectangle_t *rect) {
-    printf("Rectangle (%d, %d), width = %d, height = %d",
+    printf("Rectangle (%d, %d), width = %d, height = %d\n",
         rect->p.x, rect->p.y, rect->width, rect->height);
 }
 
 void display_triangle(const struct triangle_t *t) {
-    printf("Triangle p1(%d, %d), p2(%d, %d), p3(%d, %d)",
+    printf("Triangle p1(%d, %d), p2(%d, %d), p3(%d, %d)\n",
         t->p1.x, t->p1.y, t->p2.x, t->p2.y, t->p3.x, t->p3.y);
 }
 
 void display_circle(const struct circle_t *circ) {
-    printf("Circle (%d, %d), radius = %f",
+    printf("Circle (%d, %d), radius = %f\n",
         circ->c.x, circ->c.y, circ->r);
 }
 
@@ -60,15 +133,23 @@ void display_figure(struct figure_t *f) {
 }
 
 int compare_figures_by_area(const void* f, const void* g) {
-    return (int) (area_figure((const struct figure_t*)f) - area_figure((const struct figure_t*)g));
+
+    struct figure_t **f1 = (struct figure_t **) f;
+    struct figure_t **f2 = (struct figure_t **) g;
+
+    return (int) area_figure(*f2) - (int) area_figure(*f1);
 }
 
 int sort_by_area(struct figure_t **figures, int size) {
+    if (figures == NULL || size < 0)
+        return 1;
+
     struct figure_t* current_figure = *figures;
+
     for (int i = 0; i < size; i++) {
-        if (current_figure->type != TRIANGLE && current_figure->type != RECTANGLE && current_figure->type != CIRCLE) {
+        if (area_figure(current_figure) <= 0)
             return 1;
-        }
+        current_figure++;
     }
 
     qsort(figures, size, sizeof(struct figure_t*), compare_figures_by_area);
--- 03union/task-3-10/main.c
+++ 03union/task-3-10/main.c
@@ -1,19 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "figure.h"
 
 int main(void) {
-    struct rectangle_t r;
-    r.p.x = 0;
-    r.p.y = 0;
-    r.height = 10;
-    r.width = 10;
+    // struct figure_t figures[] = { { .type = CIRCLE, .circ = { .c = { 10, -4 }, .r = 6.621597429597 } }, { .type = TRIANGLE, .triangle = { .p1 = { -19, -20 }, .p2 = { 17, 8 }, .p3 = { -7, -11 } } }, { .type = RECTANGLE, .rect = { .p = { 4, -9 }, .width = 18, .height = 45 } } };
+    // struct figure_t *ptr[3];
+    //
+    // for (int i = 0; i < 3; ++i)
+    //     ptr[i] = &figures[i];
+    //
+    // printf("------------TABLICA FIGUR DO POSORTOWANIA-----------\n");
+    //
+    // for (int i = 0; i < 3; ++i)
+    // {
+    //     printf("%.4f ", area_figure(figures  + i));
+    //     display_figure(figures  + i);
+    // }
+    //
+    // printf("------------TABLICA FIGUR PO SORTOWANIU-----------\n");
+    //
+    // // qsort(ptr, 3, sizeof(ptr[0]), compare_figures_by_area);
+    // sort_by_area(ptr, 3);
+    //
+    // // test_error(res == 0, "Funkcja sort_by_area powinna zwrócić %d, a zwróciła %d", 0, res);
+    //
+    // struct figure_t expected_figures[] = { { .type = RECTANGLE, .rect = { .p = { 4, -9 }, .width = 18, .height = 45 } }, { .type = CIRCLE, .circ = { .c = { 10, -4 }, .r = 6.621597429597 } }, { .type = TRIANGLE, .triangle = { .p1 = { -19, -20 }, .p2 = { 17, 8 }, .p3 = { -7, -11 } } } };
+    //
+    // for (int i = 0; i < 3; ++i)
+    // {
+    //     printf("%d %.4f ", i, area_figure(ptr[i]));
+    //     display_figure(ptr[i]);
+    // }
+    //
+    // printf("------------OCZEKIWANA TABLICA FIGUR-----------\n");
+    //
+    // for (int i = 0; i < 3; ++i)
+    // {
+    //     printf("%d %.4f ", i, area_figure(expected_figures + i));
+    //     display_figure(expected_figures  + i);
+    // }
+    //
+    // printf("------------\n");
 
-    struct figure_t f;
-    f.type = RECTANGLE;
-    f.rect = r;
-
-    printf("%f\n", area_figure(&f));
-    display_figure(&f);
 
     return 0;
 }
