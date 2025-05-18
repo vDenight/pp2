fcf2cb0da3803ca400972476dae0b66ee9daa722
vDenight <mikrybczynski@gmail.com>
2025-05-18 14:28:35 +0200

    task 5 - 5 wrote functions and main

--- 05dynmemo2/task-5-5/main.c
+++ 05dynmemo2/task-5-5/main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-#include "matrix.h"
+#include <stdlib.h>
+#include <string.h>
+
+#include "matrix_utils.h"
 
 enum main_code {
     OK = 0,
     INCORRECT_INPUT = 1,
     INCORRECT_INPUT_DATA = 2,
-    ALLOC_FAIL = 8
+    ALLOC_FAIL = 8,
+    UNSUPPORTED_FAIL_FORMAT = 5,
+    CANT_CREATE_FILE = 7
 };
 
+enum file_format {
+    BINARY,
+    TEXT,
+    UNSUPPORTED
+};
+
+enum file_format read_format(char* filename);
+
 int main(void) {
-    struct matrix_t my_matrix = {0};
+    struct matrix_t* matrix_point = {0};
 
     printf("Podaj szerokość i wysokość: ");
 
@@ -20,27 +33,80 @@ int main(void) {
         printf("Incorrect input");
         return INCORRECT_INPUT;
     }
-
-    int create_val = matrix_create(&my_matrix, width, height);
-
-    if (create_val == CREATE_WRONG_INPUT) {
+    if (width < 1 || height < 1) {
         printf("Incorrect input data");
         return INCORRECT_INPUT_DATA;
     }
-    if (create_val == CREATE_ALLOC_FAIL) {
+
+    matrix_point = matrix_create_struct(width, height);
+
+    if (matrix_point == NULL) {
         printf("Failed to allocate memory");
         return ALLOC_FAIL;
     }
 
     printf("Podaj wartości: ");
-    int read_val = matrix_read(&my_matrix);
+    int read_val = matrix_read(matrix_point);
     if (read_val == READ_WRONG_FORMAT) {
         printf("Incorrect input");
-        matrix_destroy(&my_matrix);
+        matrix_destroy_struct(&matrix_point);
         return INCORRECT_INPUT;
     }
-    matrix_display(&my_matrix);
-    matrix_destroy(&my_matrix);
+
+    struct matrix_t* t_matrix_point = matrix_transpose(matrix_point);
+
+    if (t_matrix_point == NULL) {
+        printf("Failed to allocate memory");
+        matrix_destroy_struct(&matrix_point);
+        return ALLOC_FAIL;
+    }
+    matrix_destroy_struct(&matrix_point);
+
+    char* buffer = malloc(41 * sizeof(char));
+    if (buffer == NULL) {
+        printf("Failed to allocate memory");
+        matrix_destroy_struct(&t_matrix_point);
+        return ALLOC_FAIL;
+    }
+
+    printf("Podaj sciezke do pliku: ");
+    scanf("%40s", buffer);
+
+    enum file_format format = read_format(buffer);
+    int save_result = 0;
+    switch (format) {
+        case UNSUPPORTED:
+            printf("Unsupported file format");
+            free(buffer);
+            matrix_destroy_struct(&t_matrix_point);
+            return UNSUPPORTED_FAIL_FORMAT;
+        case BINARY:
+            save_result = matrix_save_b(t_matrix_point, buffer);
+            break;
+        case TEXT:
+            save_result = matrix_save_t(t_matrix_point, buffer);
+            break;
+    }
+
+    free(buffer);
+    matrix_destroy_struct(&t_matrix_point);
+
+    if (save_result == SAVE_FILE_OPEN_ERROR) {
+        matrix_destroy_struct(&t_matrix_point);
+        printf("Couldn't create file");
+    }
+
+    printf("File saved");
 
     return OK;
+}
+
+enum file_format read_format(char* filename) {
+    if (filename == NULL) return UNSUPPORTED;
+
+    char* dot = strrchr(filename, '.');
+    if (dot == NULL) return UNSUPPORTED;
+    if (strcmp(dot + 1, "bin") == 0) return BINARY;
+    if (strcmp(dot + 1, "txt") == 0) return TEXT;
+    return UNSUPPORTED;
 }
--- 05dynmemo2/task-5-5/matrix_utils.h
+++ 05dynmemo2/task-5-5/matrix_utils.h
@@ -13,8 +13,17 @@ struct matrix_t{
 
 int matrix_create(struct matrix_t *m, int width, int height);
 int matrix_read(struct matrix_t *m);
-void matrix_display(const struct matrix_t *m);
+// void matrix_display(const struct matrix_t *m);
 void matrix_destroy(struct matrix_t *m);
+
+struct matrix_t* matrix_create_struct(int width, int height);
+
+void matrix_destroy_struct(struct matrix_t **m);
+
+struct matrix_t* matrix_transpose(const struct matrix_t *m);
+
+int matrix_save_b(const struct matrix_t *m, const char *filename);
+int matrix_save_t(const struct matrix_t *m, const char *filename);
 
 enum matrix_create_code {
     CREATE_OK = 0,
@@ -26,6 +35,13 @@ enum matrix_read_code {
     READ_OK = 0,
     READ_WRONG_INPUT = 1,
     READ_WRONG_FORMAT = 2
+};
+
+enum matrix_save_code {
+    SAVE_OK = 0,
+    SAVE_WRONG_INPUT = 1,
+    SAVE_FILE_OPEN_ERROR = 2,
+    SAVE_FAIL = 3
 };
 
 #endif //MATRIX_H
