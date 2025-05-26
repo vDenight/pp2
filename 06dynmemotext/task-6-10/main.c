0be4596cad2343a1eadcb75ec7eef579077557fb
vDenight <mikrybczynski@gmail.com>
2025-05-26 15:52:59 +0200

    task 6 - 10 - done

--- 06dynmemotext/task-6-10/main.c
+++ 06dynmemotext/task-6-10/main.c
@@ -11,30 +11,31 @@ int main(void) {
     int current_capacity = 50; // actually it is 49 :)
     int current_size = 0;
     char* buffer = realloc(NULL, 50);
-    memset(buffer, 0, 50);
     if (!buffer) {
         printf("Failed to allocate memory");
         return ALLOC_FAIL;
     }
+    memset(buffer, 0, 50);
+
+    printf("Podaj tekst: ");
 
     int c;
     while (1) {
         c = getchar();
         if (c == '\n') break;
 
-        if (current_size - 1 == current_capacity) {
+        if (current_size == current_capacity - 1) {
             char* temp = realloc(buffer, current_capacity + 50);
-            if (temp == NULL) {
-                printf("%s", buffer);
-                free(buffer);
-                return OK;
+            if (!temp) {
+                break;
             }
             memset(temp + current_capacity, 0, 50);
             current_capacity += 50;
             buffer = temp;
         }
 
-        *(buffer + current_size) = c;
+        *(buffer + current_size) = (char) c;
+        current_size++;
     }
 
     printf("%s", buffer);
