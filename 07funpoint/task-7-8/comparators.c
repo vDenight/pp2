21649cb45d5f62361d92479081feee0fa779c350
vDenight <mikrybczynski@gmail.com>
2025-05-29 18:38:15 +0200

    task 7 - 8 possibly working

--- 07funpoint/task-7-8/comparators.c
+++ 07funpoint/task-7-8/comparators.c
@@ -1,3 +1,54 @@
 //
 // Created by mrybczynski on 5/29/2025.
 //
+#include "comparators.h"
+
+#include <stdint.h>
+#include <stdio.h>
+
+int comp_int(const void *ptr1, const void *ptr2) {
+    int* it_1 = (int*) ptr1;
+    int* it_2 = (int*) ptr2;
+
+    return *it_1 - *it_2;
+}
+
+int comp_double(const void *ptr1, const void *ptr2) {
+    double* db_1 = (double*) ptr1;
+    double* db_2 = (double*) ptr2;
+
+    if (*db_1 > *db_2) return 1;
+    if (*db_1 < *db_2) return -1;
+    return 0;
+}
+
+int comp_point(const void *ptr1, const void *ptr2) {
+    struct point_t* p_1 = (struct point_t*) ptr1;
+    struct point_t* p_2 = (struct point_t*) ptr2;
+
+    if (p_1->x != p_2->x) {
+        return comp_double(&(p_1->x), &(p_2->x)); // if we already have it why not use it :))
+    }
+    return comp_double(&(p_1->y), &(p_2->y));
+}
+
+int sort(const void *tab, int tab_size, int elem_size, int (*comp)(const void*, const void*)) {
+    if (tab == NULL || tab_size < 1 || elem_size < 1 || comp == NULL) return 1;
+
+    uint8_t* byte_tab = (uint8_t*) tab;
+    uint8_t temp_byte;
+
+    for (int i = 0; i < tab_size; i++) {
+        for (int j = 0; j < tab_size - 1; j++) {
+            if (comp(byte_tab + (j * elem_size), byte_tab + ((j + 1) * elem_size)) > 0) {
+                for (int k = 0; k < elem_size; k++) {
+                    temp_byte = *(byte_tab + (j * elem_size) + k);
+                    *(byte_tab+ (j * elem_size) + k) = *(byte_tab + ((j + 1) * elem_size) + k);
+                    *(byte_tab + ((j + 1) * elem_size) + k) = temp_byte;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
--- 07funpoint/task-7-8/main.c
+++ 07funpoint/task-7-8/main.c
@@ -1,6 +1,127 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "comparators.h"
+
+enum data_type {
+    INT = 0,
+    DOUBLE = 1,
+    POINT = 2
+};
+
+enum main_code {
+    OK = 0,
+    INCORRECT_INPUT = 1,
+    INCORRECT_INPUT_DATA = 2,
+    ALLOC_FAIL = 8
+};
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+    int* i_pointer = {0};
+    double* d_pointer = {0};
+    struct point_t* p_pointer = {0};
+
+    printf("Enter data type: ");
+
+    int data_type;
+    if (scanf("%d", &data_type) != 1) {
+        printf("Incorrect input");
+        return INCORRECT_INPUT;
+    }
+    if (data_type < 0 || data_type > 2) {
+        printf("Incorrect input data");
+        return INCORRECT_INPUT_DATA;
+    }
+
+    printf("Enter array size: ");
+
+    int arr_size;
+    if (scanf("%d", &arr_size) != 1) {
+        printf("Incorrect input");
+        return INCORRECT_INPUT;
+    }
+    if (arr_size < 1) {
+        printf("Incorrect input data");
+        return INCORRECT_INPUT_DATA;
+    }
+
+    switch (data_type) {
+        case INT:
+            i_pointer = calloc(arr_size, sizeof(int));
+            if (!i_pointer) {
+                printf("Failed to allocate memory");
+                return ALLOC_FAIL;
+            }
+
+            printf("Enter ints: ");
+
+            for (int i = 0; i < arr_size; i++) {
+                if (scanf("%d", i_pointer + i) != 1) {
+                    free(i_pointer);
+                    printf("Incorrect input");
+                    return INCORRECT_INPUT;
+                }
+            }
+
+            sort(i_pointer, arr_size, sizeof(int), comp_int);
+
+            for (int i = 0; i < arr_size; i++) {
+                printf("%d ", *(i_pointer + i));
+            }
+
+            free(i_pointer);
+            return OK;
+        case DOUBLE:
+            d_pointer = calloc(arr_size, sizeof(double));
+            if (!d_pointer) {
+                printf("Failed to allocate memory");
+                return ALLOC_FAIL;
+            }
+
+            printf("Enter doubles: ");
+
+            for (int i = 0; i < arr_size; i++) {
+                if (scanf("%lf", d_pointer + i) != 1) {
+                    free(d_pointer);
+                    printf("Incorrect input");
+                    return INCORRECT_INPUT;
+                }
+            }
+
+            sort(d_pointer, arr_size, sizeof(double), comp_double);
+
+            for (int i = 0; i < arr_size; i++) {
+                printf("%lf ", *(d_pointer + i));
+            }
+
+            free(d_pointer);
+            return OK;
+        case POINT:
+            p_pointer = calloc(arr_size, sizeof(struct point_t));
+            if (!p_pointer) {
+                printf("Failed to allocate memory");
+                return ALLOC_FAIL;
+            }
+
+            printf("Enter points: ");
+
+            for (int i = 0; i < arr_size; i++) {
+                if (scanf("%lf %lf\n", &((p_pointer + i)->x), &((p_pointer + i)->y)) != 2) {
+                    free(p_pointer);
+                    printf("Incorrect input");
+                    return INCORRECT_INPUT;
+                }
+            }
+
+            sort(p_pointer, arr_size, sizeof(struct point_t), comp_point);
+
+            for (int i = 0; i < arr_size; i++) {
+                printf("%lf %lf\n", ((p_pointer + i)->x), (p_pointer + i)->y);
+            }
+
+            free(p_pointer);
+            return OK;
+        default: return INCORRECT_INPUT;
+    }
 }
