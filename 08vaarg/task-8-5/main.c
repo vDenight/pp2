6e47317781312e61962394a682f2f5c82cb2fbab
vDenight <mikrybczynski@gmail.com>
2025-06-04 14:10:00 +0200

    task 8 - 5 before first attempt

--- 08vaarg/task-8-5/main.c
+++ 08vaarg/task-8-5/main.c
@@ -1,7 +1,125 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "text_modifier.h"
 
+enum main_code {
+    OK = 0,
+    INCORRECT_INPUT = 1,
+    INCORRECT_INPUT_DATA = 2,
+    ALLOC_FAIL = 8
+};
+
+void change_new_line_to_terminator(char* str);
+
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+
+    char* buffer;
+    buffer = calloc(1001, sizeof(char));
+    if (!buffer) {
+        printf("Failed to allocate memory");
+        return ALLOC_FAIL;
+    }
+
+    StringFuncPtr* funcs = calloc(4, sizeof(StringFuncPtr));
+    if (!funcs) {
+        free(buffer);
+        printf("Failed to allocate memory");
+        return ALLOC_FAIL;
+    }
+
+    *funcs = lower_to_upper;
+    *(funcs + 1) = upper_to_lower;
+    *(funcs + 2) = reverse_letter;
+    *(funcs + 3) = space_to_dash;
+
+    int* choices = calloc(4, sizeof(int));
+    if (!choices) {
+        free(buffer);
+        free(funcs);
+        printf("Failed to allocate memory");
+        return ALLOC_FAIL;
+    }
+
+    // all alloced
+    printf("Podaj tekst do przeksztalcenia: ");
+    fgets(buffer, 1001, stdin);
+    change_new_line_to_terminator(buffer);
+
+    int N;
+    printf("Podaj liczbe operacj do przeprowadzenia: ");
+    if (scanf("%d", &N) != 1) {
+        free(buffer);
+        free(funcs);
+        free(choices);
+        printf("Incorrect input");
+        return INCORRECT_INPUT;
+    }
+    if (N < 1 || N > 4) {
+        free(buffer);
+        free(funcs);
+        free(choices);
+        printf("Incorrect input data");
+        return INCORRECT_INPUT_DATA;
+    }
+
+    printf("Podaj wybrane operacje: ");
+    for (int i = 0; i < N; i++) {
+        if (scanf("%d", choices + i) != 1) {
+            free(buffer);
+            free(funcs);
+            free(choices);
+            printf("Incorrect input");
+            return INCORRECT_INPUT;
+        }
+        if ((*(choices + i)) < 0 || (*(choices + i)) > 3) {
+            free(buffer);
+            free(funcs);
+            free(choices);
+            printf("Incorrect input data");
+            return INCORRECT_INPUT_DATA;
+        }
+    }
+    char** result = NULL;
+    switch (N) {
+        case 1:
+            result = text_modifier(buffer, N, *(funcs + *(choices + 0)));
+            break;
+        case 2:
+            result = text_modifier(buffer, N, *(funcs + *(choices + 0)), *(funcs + *(choices + 1)));
+            break;
+        case 3:
+            result = text_modifier(buffer, N, *(funcs + *(choices + 0)), *(funcs + *(choices + 1)), *(funcs + *(choices + 2)));
+            break;
+        case 4:
+            result = text_modifier(buffer, N, *(funcs + *(choices + 0)), *(funcs + *(choices + 1)), *(funcs + *(choices + 2)), *(funcs + *(choices + 3)));
+            break;
+        default: break;
+    }
+
+    if (!result) {
+        free(buffer);
+        free(funcs);
+        free(choices);
+        printf("Failed to allocate memory");
+        return ALLOC_FAIL;
+    }
+
+    // print result
+    for (int i = 0; i < N; i++) {
+        printf("%s\n", *(result + i));
+    }
+
+    free_texts(result);
+    free(funcs);
+    free(choices);
+    free(buffer);
+    return OK;
+}
+
+void change_new_line_to_terminator(char* str) {
+    while (*str != '\n' && *str != '\0') {
+        str++;
+    }
+    *str = '\0';
 }
--- 08vaarg/task-8-5/text_modifier.c
+++ 08vaarg/task-8-5/text_modifier.c
@@ -5,9 +5,108 @@
 #include <stdio.h>
 #include "text_modifier.h"
 
-char** text_modifier(const char* text, int N, ...) { return NULL;}
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
 
-char* lower_to_upper(const char *in) { return NULL; }
-char* upper_to_lower(const char *in) { return NULL; }
-char* space_to_dash(const char *in) { return NULL; }
-char* reverse_letter(const char *in) { return NULL; }
+char** text_modifier(const char* text, int N, ...) {
+    if (text == NULL || N < 1) return NULL;
+
+    char** result = calloc(N, sizeof(char*));
+    if (!result) return NULL;
+
+    //initialize va_list
+    va_list functions;
+    va_start(functions, N);
+
+    for (int i = 0; i < N; i++) {
+        StringFuncPtr func = va_arg(functions, StringFuncPtr);
+        *(result+i) = func(text);
+        if (!(*result+i)) {
+            va_end(functions);
+            free(result);
+            return NULL;
+        }
+    }
+
+    va_end(functions);
+    return result;
+}
+
+char* lower_to_upper(const char *in) {
+    if (!in) return NULL;
+
+    char* res = calloc(strlen(in) + 1, sizeof(char));
+    if (!res) return NULL;
+
+    for (int i = 0; i < strlen(in); i++) {
+        *(res + i) = (char) toupper(*(in + i));
+    }
+
+    return res;
+}
+
+char* upper_to_lower(const char *in) {
+    if (!in) return NULL;
+
+    char* res = calloc(strlen(in) + 1, sizeof(char));
+    if (!res) return NULL;
+
+    for (int i = 0; i < strlen(in); i++) {
+        *(res + i) = (char) tolower(*(in + i));
+    }
+
+    return res;
+}
+
+char* space_to_dash(const char *in) {
+    if (!in) return NULL;
+
+    char* res = calloc(strlen(in) + 1, sizeof(char));
+    if (!res) return NULL;
+
+    for (int i = 0; i < strlen(in); i++) {
+        if (*(in + i) == ' ') {
+            *(res + i) = '_';
+        } else {
+            *(res + i) = *(in + i);
+        }
+    }
+
+    return res;
+}
+
+char reverse_char(char c);
+
+char* reverse_letter(const char *in) {
+    if (!in) return NULL;
+
+    char* res = calloc(strlen(in) + 1, sizeof(char));
+    if (!res) return NULL;
+
+    for (int i = 0; i < strlen(in); i++) {
+        *(res + i) = reverse_char(*(in + i));
+    }
+
+    return res;
+}
+
+char reverse_char(char c) {
+    if (!isalpha(c)) return c;
+
+    if (isupper(c)) {
+        return (char) ('Z' - c + 'A'); // uppercase
+    }
+
+    return (char) ('z' - c + 'a'); // lowercase
+}
+
+void free_texts(char **texts) {
+    char** temp = texts;
+    while (*temp) {
+        free(*temp);
+        temp++;
+    }
+    free(texts);
+}
