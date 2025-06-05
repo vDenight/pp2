4c3a383a3678a5fdb9189a54f2b2b4a55192372a
vDenight <mikrybczynski@gmail.com>
2025-06-05 14:48:45 +0200

    task 9 - 1 initial setup

---
+++ 09bitmacr/task-9-1/bit_compar.c
@@ -0,0 +1,6 @@
+//
+// Created by mrybczynski on 6/5/2025.
+//
+
+int is_equal(int a, int b);
+int is_negative(int value);
---
+++ 09bitmacr/task-9-1/bit_compar.h
@@ -0,0 +1,11 @@
+//
+// Created by mrybczynski on 6/5/2025.
+//
+
+#ifndef BIT_COMPAR_H
+#define BIT_COMPAR_H
+
+int is_equal(int a, int b);
+int is_negative(int value);
+
+#endif //BIT_COMPAR_H
