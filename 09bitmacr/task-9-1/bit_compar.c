5aececeeebe7b1cdd9b4d0403b418ec2dd9b8f7b
vDenight <mikrybczynski@gmail.com>
2025-06-05 15:17:30 +0200

    task 9 - 1 done

--- 09bitmacr/task-9-1/bit_compar.c
+++ 09bitmacr/task-9-1/bit_compar.c
@@ -2,5 +2,19 @@
 // Created by mrybczynski on 6/5/2025.
 //
 
-int is_equal(int a, int b);
-int is_negative(int value);
+int is_equal(int a, int b) {
+    int xnor_result = ~(a ^ b);
+    unsigned int bit = 1U;
+
+    while (bit) {
+        if (!(xnor_result & bit)) return 0;
+        bit <<= 1;
+    }
+    return 1;
+}
+
+int is_negative(int value) {
+    unsigned int sign_bit = 1U << 31;
+    if (sign_bit & value) return 1;
+    return 0;
+}
--- 09bitmacr/task-9-1/main.c
+++ 09bitmacr/task-9-1/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include "bit_compar.h"
+
+enum main_code {
+    OK = 0,
+    INCORRECT_INPUT = 1
+};
 
 int main(void) {
-    printf("Hello, World!\n");
-    return 0;
+    int a, b;
+    printf("Podaj dwie liczby: ");
+    if (!scanf("%d", &a)) {
+        printf("Incorrect input");
+        return INCORRECT_INPUT;
+    }
+
+    if (!scanf("%d", &b)) {
+        printf("Incorrect input");
+        return INCORRECT_INPUT;
+    }
+
+    if (is_equal(a, b)) printf("rowne\n");
+    else printf("nierowne\n");
+
+    if (is_negative(a)) printf("ujemna ");
+    else printf("nieujemna ");
+
+    if (is_negative(b)) printf("ujemna\n");
+    else printf("nieujemna\n");
+
+    return OK;
 }
