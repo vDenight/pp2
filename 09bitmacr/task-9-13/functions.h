b98578dd28b3a409b9aed01e58d9db784be1cfe5
vDenight <mikrybczynski@gmail.com>
2025-06-06 11:50:47 +0200

    task 9 - 13 done

--- 09bitmacr/task-9-13/functions.h
+++ 09bitmacr/task-9-13/functions.h
@@ -6,17 +6,46 @@
 #define FUNCTIONS_H
 
 #define COUNT_BITS(value, pointer_to_bits_set_counter, pointer_to_bits_cleared_counter) \
-    int val_bit_size = (int) sizeof(value) * 8; \
-    int bits_set = 0; \
-    int bits_cleared = 0; \
-    \
-    for(int i = 0; i < val_bit_size; i++) { \
-        if (value & (1U << i)) bits_set++; \
-        else bits_cleared++; \
+    { \
+        int val_bit_size = (int) sizeof(value) * 8; \
+        *pointer_to_bits_set_counter = 0; \
+        *pointer_to_bits_cleared_counter = 0; \
+        \
+        for(int i = 0; i < val_bit_size; i++) { \
+            if (value & (1UL << i)) (*pointer_to_bits_set_counter)++; \
+            else (*pointer_to_bits_cleared_counter)++; \
+        } \
+        \
     } \
-    \
-    *(pointer_to_bits_set_counter) = bits_set; \
-    *(pointer_to_bits_cleared_counter) = bits_cleared; \
+
+# define DISPLAY_BITS(value) \
+    { \
+        int val_bit_size = (int) sizeof(value) * 8; \
+        \
+        for(int i = val_bit_size - 1; i >= 0; i--) { \
+            if (value & (1UL << i)) putchar('1'); \
+            else putchar('0'); \
+        } \
+    } \
+
+#define REVERSE_BITS(pointer_to_variable) \
+    { \
+        int val_bit_size = (int) sizeof(*pointer_to_variable) * 8; \
+        \
+        _Bool temp1, temp2; \
+        for(int i = 0; i < val_bit_size / 2; i++) { \
+            int front_index = i; \
+            int back_index = val_bit_size - i - 1; \
+            if (*pointer_to_variable & (1UL << front_index)) temp1 = 1; \
+            else temp1 = 0; \
+            if (*pointer_to_variable & (1UL << back_index)) temp2 = 1; \
+            else temp2 = 0; \
+            if (temp1 == 1) *pointer_to_variable = *pointer_to_variable | (1UL << back_index); \
+            else *pointer_to_variable = *pointer_to_variable & ~(1UL << back_index); \
+            if (temp2 == 1) *pointer_to_variable = *pointer_to_variable | (1UL << front_index); \
+            else *pointer_to_variable = *pointer_to_variable & ~(1UL << front_index); \
+        } \
+    } \
 
 
 #endif //FUNCTIONS_H
--- 09bitmacr/task-9-13/main.c
+++ 09bitmacr/task-9-13/main.c
@@ -1,11 +1,77 @@
+#include <stdint.h>
 #include <stdio.h>
 
 #include "functions.h"
 
-int main(void) {
-    int bits_s;
-    int bits_c;
-    int minus_one = -1;
-    int plus_one = 1;
+enum main_code {
+    OK = 0,
+    INCORRECT_INPUT = 1,
+    INCORRECT_INPUT_DATA = 2
+};
 
+int main(void) {
+
+    int d_type;
+
+    printf("Enter data type: ");
+    if (scanf("%d", &d_type) != 1) {
+        printf("Incorrect input");
+        return INCORRECT_INPUT;
+    }
+    if (d_type < 0 || d_type > 2) {
+        printf("Incorrect input data");
+        return INCORRECT_INPUT_DATA;
+    }
+
+    int num_set;
+    int num_cleared;
+
+    printf("Enter an integer number: ");
+    switch (d_type) {
+        case 0: {
+            unsigned int num = {0};
+            if (scanf("%u", &num) != 1) {
+                printf("Incorrect input");
+                return INCORRECT_INPUT;
+            }
+            COUNT_BITS(num, &num_set, &num_cleared);
+            printf("%d\n%d\n", num_set, num_cleared);
+            DISPLAY_BITS(num);
+            printf("\n");
+            REVERSE_BITS(&num);
+            DISPLAY_BITS(num);
+            break;
+        }
+        case 1: {
+            unsigned short num = {0};
+            if (scanf("%hu", &num) != 1) {
+                printf("Incorrect input");
+                return INCORRECT_INPUT;
+            }
+            COUNT_BITS(num, &num_set, &num_cleared);
+            printf("%d\n%d\n", num_set, num_cleared);
+            DISPLAY_BITS(num);
+            printf("\n");
+            REVERSE_BITS(&num);
+            DISPLAY_BITS(num);
+            break;
+        }
+        case 2: {
+            uint64_t num = {0};
+            if (scanf("%lu", &num) != 1) {
+                printf("Incorrect input");
+                return INCORRECT_INPUT;
+            }
+            COUNT_BITS(num, &num_set, &num_cleared);
+            printf("%d\n%d\n", num_set, num_cleared);
+            DISPLAY_BITS(num);
+            printf("\n");
+            REVERSE_BITS(&num);
+            DISPLAY_BITS(num);
+            break;
+        }
+        default: return 2137;
+    }
+
+    return OK;
 }
