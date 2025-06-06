6ec8ea7c0c685eccbf9e91a3f32401e74c993b8c
vDenight <mikrybczynski@gmail.com>
2025-06-06 11:51:03 +0200

    task 9 - 15 fighting the compilation errors

---
+++ 09bitmacr/task-9-15/functions.h
@@ -0,0 +1,80 @@
+//
+// Created by mrybczynski on 6/6/2025.
+//
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#define DEFINE_ARRAY(TYPE) \
+    struct array_##TYPE##_t { \
+        int size; \
+        int capacity; \
+        TYPE *data; \
+    }; \
+
+#define CREATE_ARRAY(TYPE) \
+    struct array_##TYPE##_t *create_array_##TYPE(int size) { \
+        struct array_##TYPE##_t* arr = calloc(1, sizeof(struct array_##TYPE##_t)); \
+        if (!arr) return NULL; \
+        arr->data = calloc(size, sizeof(TYPE)); \
+        if (!arr->data) { free(arr); return NULL; } \
+        \
+        arr->size = 0; \
+        arr->capacity = size; \
+        \
+        return arr; \
+    } \
+
+#define FREE_ARRAY(TYPE) \
+    void free_array_##TYPE(struct array_##TYPE##_t *array) { \
+        if (!array) return; \
+        if (array->data) free(array->data); \
+        free(array); \
+    } \
+
+#define SAVE_ARRAY(TYPE) \
+    int save_array_##TYPE(const struct array_##TYPE##_t *array, const char *filename) { \
+        if (!array || !array->data || !filename) return 1; \
+        \
+        FILE* f = fopen(filename, "wb"); \
+        if (!f) return 2; \
+        \
+        fwrite(&(array->size), sizeof(int), 1, f); \
+        fwrite(array->data, sizeof(TYPE), array->size, f); \
+        \
+        fclose(f); \
+        return 0; \
+    } \
+
+#define LOAD_ARRAY(TYPE) \
+    int load_array_##TYPE(struct array_##TYPE##_t **array, const char *filename) { \
+        if (!array || !filename) return 1; \
+        \
+        FILE* f = fopen(filename, "rb"); \
+        if (!f) return 2; \
+        \
+        int cap = 0; \
+        fread(&cap, sizeof(int), 1, f); \
+        if (cap < 1) { fclose(f); return 3; } \
+        \
+        struct array_##TYPE##_t *arr = create_array_##TYPE(cap); \
+        if (!arr) { fclose(f); return 4; } \
+        \
+        fread(arr->data, sizeof(TYPE), cap, f); \
+        \
+        fclose(f); \
+        return 0; \
+    } \
+
+#define SORT_ARRAY(TYPE) \
+    int compare_##TYPE(const void* a, const void* b) { \
+        return *(TYPE*)a - *(TYPE*)b; \
+    } \
+    \
+    int sort_array_##TYPE(struct array_##TYPE##_t *array) { \
+        if (!array || !array->data) return 1; \
+        qsort(array->data, array->size, sizeof(TYPE), compare_##TYPE); \
+        return 0; \
+    } \
+
+#endif //FUNCTIONS_H
